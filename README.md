87206f5731bc8dd087a924570b69d354721340ee
Tommasso Borgato <tborgato@redhat.com>
2022-03-03 12:24:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# JBoss EAP Messaging: remote queues
+
+This repo explains how-to create a MDB that consumes messages from a remote queue;
+
+We have two node:
+- node-1: is the node where the queue is
+- node-2: is the node where the MDB is; the MDB consumes messages from the queue on node-1
+
+We assume you have two JBoss EAP installations in the following folders:
+
+### jboss-eap-7.4-node-1
+
+```shell
+$ tree -L 1 jboss-eap-7.4-node-1
+jboss-eap-7.4-node-1
+├── appclient
+├── bin
+├── docs
+├── domain
+├── JBossEULA.txt
+├── jboss-modules.jar
+├── LICENSE.txt
+├── migration
+├── modules
+├── standalone
+├── version.txt
+└── welcome-content
+```
+### jboss-eap-7.4-node-2
+
+```shell
+$ tree -L 1 jboss-eap-7.4-node-2
+jboss-eap-7.4-node-2
+├── appclient
+├── bin
+├── docs
+├── domain
+├── JBossEULA.txt
+├── jboss-modules.jar
+├── LICENSE.txt
+├── migration
+├── modules
+├── standalone
+├── version.txt
+└── welcome-content
+```
+
+## node-1 setup
+
+```shell
+./jboss-eap-7.4-node-1/bin/jboss-cli.sh
+
+embed-server --server-config=standalone-full-ha.xml
+/subsystem=messaging-activemq/server=default:write-attribute(name=security-enabled,value=false)
+jms-queue add --queue-address=MDBQueue1Node1 --entries=[java:/queue/MDBQueue1Node1]
+jms-queue add --queue-address=MDBQueue2Node1 --entries=[java:/queue/MDBQueue2Node1]
+```
+
+## node-2 setup
+
+```shell
+./jboss-eap-7.4-node-2/bin/jboss-cli.sh
+
+embed-server --server-config=standalone-full-ha.xml
+/subsystem=messaging-activemq/server=default:write-attribute(name=security-enabled,value=false)
+/socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=remote-server:add(host=127.0.0.1, port=8080)
+/subsystem=messaging-activemq/server=default/http-connector=remote-http-connector:add(socket-binding=remote-server,endpoint=http-acceptor)
+/subsystem=messaging-activemq/server=default/pooled-connection-factory=remote-artemis:add(connectors=[remote-http-connector], entries=[java:/jms/remoteCF])
+```
+
+## node-1 start
+
+```shell
+./jboss-eap-7.4-node-1/bin/standalone.sh --server-config=standalone-full-ha.xml -Djboss.default.jgroups.stack=tcp -Dprogram.name=wfl1 -Djboss.node.name=wfl1 -Djboss.default.multicast.address=230.0.0.1
+```
+
+## node-2 start
+
+```shell
+./jboss-eap-7.4-node-2/bin/standalone.sh --server-config=standalone-full-ha.xml -Djboss.socket.binding.port-offset=1000 -Djboss.default.jgroups.stack=tcp -Dprogram.name=wfl2 -Djboss.node.name=wfl2 -Djboss.default.multicast.address=230.0.0.2
+```
---
+++ node-2/src/main/java/org/jboss/as/quickstarts/servlet/MDBQueue1Node1.java
@@ -0,0 +1,111 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSDestinationDefinitions;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueBrowser;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Definition of the two JMS destinations used
+ */
+@JMSDestinationDefinitions(
+    value = {
+        @JMSDestinationDefinition(
+            name = "java:/queue/MDBQueue1Node1",
+            interfaceName = "javax.jms.Queue",
+            destinationName = "MDBQueue1Node1"
+        )
+    }
+)
+
+/**
+ * <p>
+ * A simple servlet 3 as client that is capable of sending messages to a remote queue or counting messages in that queue.
+ * </p>
+ */
+@WebServlet("/MDBQueue1Node1")
+public class MDBQueue1Node1 extends HttpServlet {
+
+    private static final long serialVersionUID = -8314035702649252239L;
+
+    @Inject
+    @JMSConnectionFactory("java:/jms/remoteCF")
+    private JMSContext context;
+
+    @Resource(lookup = "java:/queue/MDBQueue1Node1")
+    private Queue queue;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        try {
+            String operation = req.getParameter("op");
+            final Destination destination = queue;
+
+            if ("send".equalsIgnoreCase(operation)) {
+                out.write("<p>Sending messages to <em>" + destination + "</em></p>");
+                out.write("<h2>The following messages will be sent to the destination:</h2>");
+                for (int i = 0; i < 10; i++) {
+                    String text = "This is message " + (i + 1);
+                    context.createProducer().send(destination, text);
+                    out.write("Message (" + i + "): " + text + "</br>");
+                }
+                out.write("<p><i>Go to your JBoss EAP server console or server log to see the result of messages processing.</i></p>");
+            } else if ("count".equalsIgnoreCase(operation)) {
+                QueueBrowser browser = context.createBrowser(queue);
+                Enumeration messages = browser.getEnumeration();
+                int MSG_COUNT = 0;
+                while(messages.hasMoreElements()){
+                    messages.nextElement();
+                    MSG_COUNT++;
+                }
+                out.write("<count>" + MSG_COUNT + "</count>");
+            } else {
+                out.write("<p>Operation unknown: please specify parameter \"op\", e.g. '?op=send', '?op=count' </p>");
+            }
+        } catch (JMSException e) {
+            out.write(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
